4a7070817ab1f16dbb2cd16f2ec773891ae2fb50
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2016-05-18 00:11:50 -0300

    Stylus Task

---
+++ workflow-stylus/tasks/stylus.js
@@ -0,0 +1,22 @@
+module.exports = function (gulp, plugins, config) {
+    const jeet = require('jeet'),
+          rupture = require('rupture'),
+          nib = require('nib'),
+          prefixer = require('autoprefixer-stylus');
+
+    plugins.browserSync = require('browser-sync').create();
+    const reload = plugins.browserSync.reload;
+
+    gulp.task('stylus', function(){
+        gulp.src(config.stylusSrcFolder + '/main.styl')
+            .pipe(plugins.plumber())
+            .pipe(plugins.stylus({
+                use: [prefixer(), jeet(), nib(), rupture()],
+                compress: true
+            }))
+            .pipe(gulp.dest(config.stylusSrcFolder))
+            .pipe(plugins.rename('styles.css'))
+            .pipe(gulp.dest(config.stylusDestFolder))
+            .pipe(reload({stream: true}));
+    });
+};
