c35c8bc499e70a0c3664949b348de37e3895bc65
Ednilson Amaral <ednilsonamaral.ti@gmail.com>
2016-05-18 00:12:03 -0300

    Watch Task

---
+++ workflow-stylus/tasks/watch.js
@@ -0,0 +1,24 @@
+module.exports = function (gulp, plugins, config) {
+    // Watch Task
+    gulp.task('watch', function () {
+        //Builds CSS
+        plugins.watch(config.stylusSrcFolder + '/**/*.styl', function () {
+            gulp.start('stylus');
+        });
+
+        //Build Pug
+        plugins.watch(config.pugSrcFolder + '/**/*.pug', function () {
+            gulp.start('pug');
+        });
+
+        //Build JS
+        plugins.watch(config.jsSrcFolder + '/**/*.js', function () {
+            gulp.start('js-compile');
+        });
+
+        //Build IMG
+        plugins.watch(config.imgSrcFolder + '/**/*.{jpg,png,gif}', function () {
+            gulp.start('imagemin');
+        });
+    });
+};
